d179fa1de66001d2973aab28fb5394ee88e10be1
tannerjt <biolsurfer85@gmail.com>
2016-12-31 11:26:58 -0800

    Add Depot from Agile development with rails 5

---
+++ depot/config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  get 'admin/index'
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  resources :users
+  resources :orders
+  resources :line_items
+  resources :carts
+  root 'store#index', as: 'store_index'
+
+  resources :products do
+    get :who_bought, on: :member
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
